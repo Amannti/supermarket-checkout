f0f3f13279ae8cf8b46275fbed633be3644ee183
Amannti <TilAmann96@live.de>
2025-01-22 21:48:39 +0100

    Add home page and implemented the item view

---
+++ frontend/src/app/home/item-container/item-container.component.ts
@@ -0,0 +1,19 @@
+import {Component, input} from '@angular/core';
+import {ItemWithOffersDto} from "../../dto/ItemWithOffersDto";
+import {ItemCardComponent} from "./item-card/item-card.component";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-item-container',
+  standalone: true,
+  imports: [
+    ItemCardComponent,
+    NgIf,
+    NgForOf
+  ],
+  templateUrl: './item-container.component.html',
+  styleUrl: './item-container.component.scss'
+})
+export class ItemContainerComponent {
+  items = input.required<ItemWithOffersDto[] | undefined>();
+}
