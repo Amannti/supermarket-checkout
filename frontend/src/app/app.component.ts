e46adaab7c4097bc0449357dd2d9d5d352dec092
Amannti <TilAmann96@live.de>
2025-01-22 16:32:38 +0100

    Add request from frontend and webconfig to allow FE calls

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Supermarket Checkout';
+
+  private readonly http = inject(HttpClient);
+
+  testData = toSignal(this.http.get('http://localhost:8080/api/items'));
 }
